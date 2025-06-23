6011a2b7bb96fcf1db54ef5034b0f363fd454b5f
Gabriel González Rodríguez <gabrielgr01@hotmail.com>
2025-06-23 00:54:24 -0600

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -12,29 +12,37 @@ import os
 
 RUN_AREA = os.getcwd() # Directory where the program is run
 RESULTS_DIR_NAME = "results"
-RESULTS_DIR_PATH = str(RUN_AREA) + f"\\{RESULTS_DIR_NAME}"
+RESULTS_DIR_PATH = os.path.join(RUN_AREA, RESULTS_DIR_NAME)
 DATA_DIR_NAME = "data"
-DATA_DIR_PATH = str(RUN_AREA) + f"\\{DATA_DIR_NAME}"
+DATA_DIR_PATH = os.path.join(RUN_AREA, DATA_DIR_NAME)
 
 DATASET_CSV_NAME = "dataset.csv"
-DATASET_CSV_PATH = str(DATA_DIR_PATH) + f"\\{DATASET_CSV_NAME}"
+DATASET_CSV_PATH = os.path.join(DATA_DIR_PATH, DATASET_CSV_NAME)
 
-# General
-VERBOSE = False  
-DEBUG = False    
+MODEL_SAVE_NAME = "model.keras"
+MODEL_SAVE_PATH = os.path.join(RESULTS_DIR_PATH, MODEL_SAVE_NAME)
+MAX_CONFIG_SAVE_NAME = "max_config.csv"
+MAX_CONFIG_SAVE_PATH = os.path.join(RESULTS_DIR_PATH, MAX_CONFIG_SAVE_NAME)
+MIN_CONFIG_SAVE_NAME = "min_config.csv"
+MIN_CONFIG_SAVE_PATH = os.path.join(RESULTS_DIR_PATH, MIN_CONFIG_SAVE_NAME)
+MODEL_CONFIG_PATH_LIST = [MAX_CONFIG_SAVE_PATH, MIN_CONFIG_SAVE_PATH]
 
-# PID Gains
-PID_KP = 50 # Proportional Gain
-PID_KI = 5  # Integral Gain
-PID_KD = 10 # Derivative Gain
+SIM_T_STOP = 20
+SIM_T_SAMPLES = SIM_T_STOP * 10
 
+VERBOSE = False
+DEBUG = False
+
+PID_KP = 50         # Proportional Gain for the PID
+PID_KI = 5          # Integral Gain for the PID
+PID_KD = 10         # Derivative Gain for the PID
+TARGET_ANGLE = 0    # [rad] Target angle for the pendulum control
 DYNAMIC_INPUT_PARAMS = [1.0,    # mass [kg]
                         1.0,    # length [m]
                         9.81,   # gravity [m/s^2]
                         0.1,    # friction coefficient
                         0.0     # external force (perturbation) [N]
                         ]
-TARGET_ANGLE = 0 # [rad] Target angle for the pendulum control
 
 
 ##### HYPERPARAMETERS #####
@@ -42,10 +50,12 @@ TARGET_ANGLE = 0 # [rad] Target angle for the pendulum control
 # Neural Network
 NUM_INPUT_NEURONS = 2   # Number of neurons of the input layer
 NUM_OUTPUT_NEURONS = 1  # Number of neurons of the output put layer
-
-# List of neurons of each hidden layer. Example: [5, 5] represents two
-# hidden layers of 5 neurons each.
-NUM_HIDDEN_NEURONS = [5, 5]
+NUM_HIDDEN_NEURONS = [6,] # List of neurons of each hidden layer. 
+                          # Example: [5, 5] represents two hidden 
+                          # layers of 5 neurons each.
+INPUT_ACT_FUNCTION = "tanh"
+HIDDEN_ACT_FUNCTION = "tanh"
+OUTPUT_ACT_FUNCTION = "linear"
 
 # Evolutionary Algorithm
 POPULATION_SIZE = 100    #
