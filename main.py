49176c656a336b2e75ad4a88479505aba2ce252c
Gabriel González Rodríguez <gabrielgr01@hotmail.com>
2025-06-24 01:04:39 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 ##### IMPORTS #####
 
 # Third party imports
-import pandas as pd
 
 # Built-in imports
 
 # Local imports
 from modules.config import *
+import modules.utils as util
 import modules.simulation as sim
 import modules.network as net
 import modules.evolution as evol
-import modules.utils as utils
 
 
 ##### GLOBAL VARIABLES #####
@@ -19,7 +18,7 @@ import modules.utils as utils
 ##### FUNCTIONS DEFINITION #####
 
 def train_new_model(dataset_dataframe):
-    utils.create_directory(RESULTS_DIR_PATH, backup=False, overwrite=False)
+    util.create_directory(RESULTS_DIR_PATH, backup=False, overwrite=False)
     
     # Creates the neural network model
     net_model = net.build_model(num_input_neurons=NUM_INPUT_NEURONS,
@@ -31,34 +30,30 @@ def train_new_model(dataset_dataframe):
     # Runs the evolutionary algorithm to get the network's weigths
     best_ind, best_ind_fitness = evol.run_evolutionary_algorithm(net_model, dataset_dataframe)
 
-    print("Best Individual: ", best_ind)
-    print("Fitness: ", float(best_ind_fitness))
-
     net.set_model_weights(net_model, best_ind)
+    
+    print("Best Individual: ", best_ind)
+    print("Fitness: ", best_ind_fitness)
     
     return net_model
 
 
 ##### MAIN EXECUTION #####
 
-### TO TRAIN AND SAVE A NEW NEURAL NETWORK ###
-#_, dataset_df = sim.generate_dataset(n_sims = 20, data_path=DATA_DIR_PATH, option="PID")  # Generates new dataset of n_sims simulations
-#dataset_df = sim.load_dataset(DATASET_CSV_PATH)     # Loads dataset from .csv
+## Generates new dataset of n_sims simulations
+#_, dataset_df = sim.generate_dataset(n_sims = 20, data_path=DATA_DIR_PATH, option="PID")
 
+## Loads a dataset from a .csv file
+#dataset_df = sim.load_dataset(DATASET_CSV_PATH)
+
+## Trains the new neural network model
 #new_model = train_new_model(dataset_df)
 
+## Saves the neural network model
 #net.save_model(new_model, MODEL_SAVE_PATH)
-##############################################
 
-# Loads the network model
+## Loads the neural network model
 net_model = net.load_model(MODEL_SAVE_PATH)
 
-# Uses the network model to predict
-########################
-#theta = 0.2
-#vel = -0.5
-#torque = net.model_predict(net_model, theta, vel)
-#print ("Torque: ", torque)
-########################
-
+## Simulates the neural network model
 _, network_sim_df = sim.generate_dataset(n_sims = 10, data_path=RESULTS_DIR_PATH, option="NETWORK", model=net_model)
